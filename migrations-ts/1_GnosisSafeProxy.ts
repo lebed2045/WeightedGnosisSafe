143c1b2160569bd12ee99ae958b21c24775e6eb4
lebed2045 <lebed2045@protonmail.com>
2021-10-24 13:19:28 +0100

    q

--- migrations-ts/1_GnosisSafeProxy.ts
+++ migrations-ts/1_GnosisSafeProxy.ts
@@ -4,6 +4,7 @@ const SafeProxy = artifacts.require("GnosisSafeProxy");
 const GnosisSafe = artifacts.require("GnosisSafe");
 const WeightModule = artifacts.require("WeightModule");
 
+// @ts-ignore
 import {getDeployedAddresses} from "./deployed_addresses";
 import {GnosisSafeInstance, GnosisSafeProxyInstance} from "../types/truffle-contracts";
 
@@ -43,27 +44,28 @@ module.exports = function (deployer, network, accounts) {
 
         console.log("await WeightModule.setupOwnersWithPoints");
         // for the multisig call of:
-        const contractModule = new web3.eth.Contract(wModule.abi, wModule.address);
-        const rawTx = contractModule.methods.setupOwnersWithPoints(
-            DEPLOYED.DEVELOPERS,
-            DEPLOYED.POINTS,
-            40
-        ).encodeABI();
-        console.log("rawTx", rawTx);
 
-        const ethAdapterOwner1 = new Web3Adapter({
-            web3,
-            signerAddress: deployer_acc
-        })
-
-        const safeSdk: Safe = await Safe.create({ethAdapter: ethAdapterOwner1, safeAddress: safeInstance.address})
-
-        const safeTransaction = await safeSdk.createTransaction(rawTx);
-
-        const owner1Signature = await safeSdk.signTransaction(safeTransaction);
-
-        const executeTxResponse = await safeSdk.executeTransaction(safeTransaction)
-        await executeTxResponse.transactionResponse?.wait()
+        // const contractModule = new web3.eth.Contract(wModule.abi, wModule.address);
+        // const rawTx = contractModule.methods.setupOwnersWithPoints(
+        //     DEPLOYED.DEVELOPERS,
+        //     DEPLOYED.POINTS,
+        //     40
+        // ).encodeABI();
+        // console.log("rawTx", rawTx);
+        //
+        // const ethAdapterOwner1 = new Web3Adapter({
+        //     web3,
+        //     signerAddress: deployer_acc
+        // })
+        //
+        // const safeSdk: Safe = await Safe.create({ethAdapter: ethAdapterOwner1, safeAddress: safeInstance.address})
+        //
+        // const safeTransaction = await safeSdk.createTransaction(rawTx);
+        //
+        // const owner1Signature = await safeSdk.signTransaction(safeTransaction);
+        //
+        // const executeTxResponse = await safeSdk.executeTransaction(safeTransaction)
+        // await executeTxResponse.transactionResponse?.wait()
     });
 } as Truffle.Migration;
 export {}
